f44e3a28e4517c28b08d7482a1952bf17f210c50
Mykhaylo Kozachok <Mykhaylo_Kozachok@epam.com>
2017-10-18 17:53:27 +0300

    Updated store server.

--- shop-server/index.js
+++ shop-server/index.js
@@ -1,21 +1,23 @@
 var express = require('express')
 var fs = require('fs');
+var cors = require('cors');
+var bodyParser = require('body-parser');
 var app = express();
 
-var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(cors());
 
 var shortid = require('shortid');
 
 app.get('/', function(req, res){
-  res.send('Hello World!');
+  res.send('Works!');
 });
 
 app.get('/api/products', function (req, res) {
   fs.readFile('./products.json', 'utf8', function (err, data) {
     if (err) throw err;
-    res.json(data);
+    res.send(data);
   });
 });
 
@@ -23,54 +25,66 @@ app.get('/api/products/:id', function (req, res) {
   fs.readFile('./products.json', 'utf8', function (err, data) {
     if (err) throw err;
 
-    var parsedData = JSON.parse(data);
-    var products = parsedData.products;
+    var products = JSON.parse(data);
     var productId = req.params.id;
-    if (products && productId && products[productId]){
-      res.json(products[productId]);
-    } else {
-      res.json({ error: "No products found with id " + productId });
+    if (products && productId){
+      var product = products.find(
+        function(item) {
+          return item.id === productId;
+        }
+      );
+      res.json(product || null);
     }
   });
 });
 
 app.post('/api/products', function (req, res) {
   var data = JSON.parse(fs.readFileSync('./products.json', 'utf8'));
-  var newProduct = req.body.product;
+  var newProduct = JSON.parse(req.body.product);
   var randomId = shortid.generate();
 
-  data.products[randomId] = newProduct;
+  newProduct.id = randomId;
+  console.log(newProduct);
+  data.push(newProduct);
   json = JSON.stringify(data);
+
   fs.writeFile('./products.json', json, 'utf8');
-  res.send("done");
+
+  res.json(newProduct);
 });
 
 app.put('/api/products', function (req, res) {
   var data = JSON.parse(fs.readFileSync('./products.json', 'utf8'));
-  var newProduct = req.body.product;
-  var productId = req.body.id;
-  data.products[productId] = newProduct;
-  json = JSON.stringify(data);
+  var newProduct = JSON.parse(req.body.product);
+  var productId = req.body.product.id;
+
+  var updated = data.map(function(item) {
+    if (item.id === productId) {
+      return newProduct;
+    } else {
+      return item;
+    }
+  });
+  json = JSON.stringify(updated);
   fs.writeFile('./products.json', json, 'utf8');
-  res.send("done");
+
+  res.json(newProduct);
 });
 
 app.delete('/api/products/:id', function (req, res) {
   fs.readFile('./products.json', 'utf8', function (err, data) {
     if (err) throw err;
 
-    var parsedData = JSON.parse(data);
-    var products = parsedData.products;
+    var products = JSON.parse(data);
     var productId = req.params.id;
-    if (products && productId && products[productId]){
-      delete parsedData.products[productId];
-
-      json = JSON.stringify(parsedData);
-      fs.writeFile('./products.json', json, 'utf8');
-      res.send("done");
-    } else {
-      res.json({ error: "No products found with id " + productId });
+    if (products && productId){
+      var filterd = products.filter(function(item) {
+        return item.id !== productId
+      });
     }
+    json = JSON.stringify(filterd);
+    fs.writeFile('./products.json', json, 'utf8');
+    res.json(filterd);
   });
 });
 
